bd46464e4afca76356e75ea42d7cac545ca8e05a
Zabi <zbvmihai@gmail.com>
2023-03-12 15:32:38 +0000

    Add functionality to save button
    -Added the functionality to save all the details in a database using SQLite.

--- app/src/main/java/com/zabava/happyplaces/activities/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/activities/AddHappyPlaceActivity.kt
@@ -1,4 +1,4 @@
-package com.zabava.happyplaces
+package com.zabava.happyplaces.activities
 
 import android.app.Activity
 import android.app.AlertDialog
@@ -19,7 +19,9 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.zabava.happyplaces.database.DatabaseHandler
 import com.zabava.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.zabava.happyplaces.models.HappyPlaceModel
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -35,6 +37,10 @@ class AddHappyPlaceActivity : AppCompatActivity() {
     private val cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
+    private var saveImageToInternalStorage: Uri? = null
+    private var mLatitude: Double = 0.0
+    private var mLongitude: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -49,6 +55,37 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         setDate()
         addPhoto()
 
+        binding?.btnSave?.setOnClickListener {
+
+            if (binding?.etTitle?.text?.isEmpty()!!) {
+                Toast.makeText(this, "Please enter a title", Toast.LENGTH_SHORT).show()
+            }else if (binding?.etDescription?.text?.isEmpty()!!) {
+                Toast.makeText(this, "Please enter a Description", Toast.LENGTH_SHORT).show()
+            } else if (binding?.etDate?.text?.isEmpty()!!) {
+                Toast.makeText(this, "Please select a date", Toast.LENGTH_SHORT).show()
+            }else if (binding?.etLocation?.text?.isEmpty()!!) {
+                Toast.makeText(this, "Please enter a location", Toast.LENGTH_SHORT).show()
+            }else if (saveImageToInternalStorage == null){
+                Toast.makeText(this, "Please select an image", Toast.LENGTH_SHORT).show()
+            }else{
+                val happyPlaceModel = HappyPlaceModel(
+                    0,
+                    binding?.etTitle?.text.toString(),
+                    saveImageToInternalStorage.toString(),
+                    binding?.etDescription?.text.toString(),
+                    binding?.etDate?.text.toString(),
+                    binding?.etLocation?.text.toString(),
+                    mLatitude,
+                    mLongitude)
+                val dbHandler = DatabaseHandler(this)
+                val addHappyPlace = dbHandler.addHappyPlace(happyPlaceModel)
+
+                if (addHappyPlace > 0){
+                    Toast.makeText(this, "The details are inserted successfully", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
     }
 
     private fun addPhoto() {
@@ -170,18 +207,18 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         }
     }
 
-    private fun saveImageToInternalStorage(image: Bitmap): Uri{
+    private fun saveImageToInternalStorage(image: Bitmap): Uri {
         val wrapper = ContextWrapper(applicationContext)
         var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
         file = File(file, "${UUID.randomUUID()}.jpg")
 
         try {
-            val stream : OutputStream = FileOutputStream(file)
-            image.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            val stream: OutputStream = FileOutputStream(file)
+            image.compress(Bitmap.CompressFormat.JPEG, 100, stream)
             stream.flush()
             stream.close()
-            Toast.makeText(this,"Image saved!",Toast.LENGTH_SHORT).show()
-        }catch (e: IOException){
+            Toast.makeText(this, "Image saved!", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
             e.printStackTrace()
         }
         return Uri.parse(file.absolutePath)
@@ -197,7 +234,8 @@ class AddHappyPlaceActivity : AppCompatActivity() {
                     try {
                         val selectedImageBitmap = MediaStore.Images.Media
                             .getBitmap(this.contentResolver, contentURI)
-                        saveImageToInternalStorage(selectedImageBitmap)
+
+                        saveImageToInternalStorage = saveImageToInternalStorage(selectedImageBitmap)
                         binding?.ivPlaceImage?.setImageBitmap(selectedImageBitmap)
                     } catch (e: IOException) {
                         e.printStackTrace()
@@ -211,7 +249,7 @@ class AddHappyPlaceActivity : AppCompatActivity() {
             } else if (requestCode == CAMERA) {
                 val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap
                 binding?.ivPlaceImage?.setImageBitmap(thumbnail)
-                saveImageToInternalStorage(thumbnail)
+                saveImageToInternalStorage = saveImageToInternalStorage(thumbnail)
             }
         }
     }
--- app/src/main/java/com/zabava/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/activities/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zabava.happyplaces
+package com.zabava.happyplaces.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
