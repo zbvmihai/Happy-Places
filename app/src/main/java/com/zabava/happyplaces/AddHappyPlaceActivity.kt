83c7cec001d7f8dc438bd3744dfe7da22fc6aa27
Zabi <zbvmihai@gmail.com>
2023-03-12 11:04:05 +0000

    Add Happy places UI design

--- app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,19 @@ package com.zabava.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zabava.happyplaces.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private var binding:ActivityAddHappyPlaceBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setSupportActionBar(binding?.tbAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding?.tbAddPlace?.setNavigationOnClickListener {
+        finish()
+        }
     }
 }
--- app/src/main/java/com/zabava/happyplaces/MainActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.zabava.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zabava.happyplaces.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlaces?.setOnClickListener {
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
