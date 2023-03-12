1533146e7ad72a36c74c3fec7548e3c49316d929
Zabi <zbvmihai@gmail.com>
2023-03-12 11:28:19 +0000

    Add Date picker for Date field

--- app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,19 @@
 package com.zabava.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zabava.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+
     private var binding:ActivityAddHappyPlaceBinding? = null
+
+    private val cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -16,5 +24,28 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding?.tbAddPlace?.setNavigationOnClickListener {
         finish()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR,year)
+            cal.set(Calendar.MONTH,month)
+            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+            updateDateInView()
+        }
+
+        binding?.etDate?.setOnClickListener {
+            DatePickerDialog(this@AddHappyPlaceActivity,
+                dateSetListener,
+                cal.get(Calendar.YEAR),
+                cal.get(Calendar.MONTH),
+                cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
+
+    private fun updateDateInView(){
+
+        val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+        binding?.etDate?.setText(sdf.format(cal.time).toString())
+    }
+
 }
