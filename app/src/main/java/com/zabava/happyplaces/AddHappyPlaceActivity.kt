56456604357c05d7b27176e1bfc6dc770116c688
Zabi <zbvmihai@gmail.com>
2023-03-12 12:14:51 +0000

    Add Dialog for add image button and permission for read/write

--- app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/zabava/happyplaces/AddHappyPlaceActivity.kt
@@ -1,15 +1,26 @@
 package com.zabava.happyplaces
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.zabava.happyplaces.databinding.ActivityAddHappyPlaceBinding
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
 
-    private var binding:ActivityAddHappyPlaceBinding? = null
+    private var binding: ActivityAddHappyPlaceBinding? = null
 
     private val cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
@@ -22,30 +33,97 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         binding?.tbAddPlace?.setNavigationOnClickListener {
-        finish()
+            finish()
         }
 
-        dateSetListener = DatePickerDialog.OnDateSetListener {
-                _, year, month, dayOfMonth ->
-            cal.set(Calendar.YEAR,year)
-            cal.set(Calendar.MONTH,month)
-            cal.set(Calendar.DAY_OF_MONTH,dayOfMonth)
-            updateDateInView()
+        setDate()
+
+        binding?.tvAddImage?.setOnClickListener {
+            val pictureDialog = AlertDialog.Builder(this@AddHappyPlaceActivity)
+            pictureDialog.setTitle("Select Action")
+            val pictureDialogItems =
+                arrayOf("Select photo from Gallery", "Capture photo from camera")
+            pictureDialog.setItems(pictureDialogItems) { _, which ->
+                when (which) {
+                    0 -> choosePhotoFromGallery()
+                    1 -> Toast.makeText(this, "Camera selection", Toast.LENGTH_SHORT).show()
+                }
+            }
+            pictureDialog.show()
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withActivity(this).withPermissions(
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(
+                report: MultiplePermissionsReport?
+            ) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Storage/Write permissions are granted.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this@AddHappyPlaceActivity,
+                        "Permissions denied",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage(
+                "It looks like you have turned off permissions required for this feature." +
+                        " It can be enabled under application settings."
+            )
+            .setPositiveButton("Go to settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
+    private fun setDate() {
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+            binding?.etDate?.setText(sdf.format(cal.time).toString())
         }
 
         binding?.etDate?.setOnClickListener {
-            DatePickerDialog(this@AddHappyPlaceActivity,
+            DatePickerDialog(
+                this@AddHappyPlaceActivity,
                 dateSetListener,
                 cal.get(Calendar.YEAR),
                 cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH)).show()
+                cal.get(Calendar.DAY_OF_MONTH)
+            ).show()
         }
     }
-
-    private fun updateDateInView(){
-
-        val sdf = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
-        binding?.etDate?.setText(sdf.format(cal.time).toString())
-    }
-
 }
+
